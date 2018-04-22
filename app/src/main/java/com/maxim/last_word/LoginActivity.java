4ca9bc8418b3caf12c2466de45a6043eeef2de2d
zivan38 <zivan38@yandex.ru>
2018-04-23 04:22:47 +0800

    Data base

--- app/src/main/java/com/maxim/last_word/LoginActivity.java
+++ app/src/main/java/com/maxim/last_word/LoginActivity.java
@@ -45,7 +45,7 @@ public class LoginActivity extends FragmentActivity {
 
     private boolean isResumed = false;
     private static SharedPreferences bd;
-    public static boolean hasVisited;
+    //public static boolean hasVisited;
     /**
      * Scope is set of required permissions for your application
      *
@@ -65,11 +65,11 @@ public class LoginActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        bd = getSharedPreferences("savedStrings", Context.MODE_PRIVATE);
+        bd = getPreferences(MODE_PRIVATE);
         SharedPreferences sp = getSharedPreferences("savedStrings",
                 Context.MODE_PRIVATE);
         // проверяем, первый ли раз открывается программа
-        hasVisited = sp.getBoolean("hasVisited", false);
+        //hasVisited = sp.getBoolean("hasVisited", false);
 
         setContentView(R.layout.activity_start);
         VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
@@ -212,50 +212,66 @@ public class LoginActivity extends FragmentActivity {
         }
 
 
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View v = inflater.inflate(R.layout.fragment_logout, container, false);
             text = v.findViewById(R.id.text_lastword);
+            date_view = v.findViewById(R.id.textView);
 
-
-
-            if (hasVisited) {
+            try {
                 text.setText(bd.getString("textlastword", ""));
-                date_view.setText(bd.getString("lastdate", ""));
+                long lastd = bd.getLong("lastdate", 0);
+                Date last = new Date(lastd);
+                date_view.setText(last.toString());
+            }
+            catch(Exception e){
+
+            };
 
                // if (bd.getString("lastdate", "")){
 
                // }
-            }
+
 
             date_view = v.findViewById(R.id.textView);
             v.findViewById(R.id.continue_button).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                    Date date = new Date();
+                    /*Date date = new Date();
                     date_view.setText(date.toString());
                     SharedPreferences.Editor editor = bd.edit();
                     editor.putString("textlastword", text.getText().toString());
                     editor.putString("lastdate", date_view.getText().toString());
                     editor.apply();
-                    editor.commit();
+                    editor.commit();*/
 
 
 
-                    // currentTime = checkDate
-                    Date currentTime = Calendar.getInstance().getTime();
+                    /*text.setText(bd.getString("textlastword", ""));
+                    long curtime = bd.getLong("lastdate", 0);
+                    Date curdate = new Date(curtime);
+                    date_view.setText(curdate.toString());*/
+
+
+
+                    //Date currentTime = Calendar.getInstance().getTime();
                     long currentMillis = Calendar.getInstance().getTimeInMillis();
-                    Date checkDate = new Date (currentMillis);
+                    //Date checkDate = new Date (currentMillis);
 
 
+                    SharedPreferences.Editor ed = bd.edit();
+                    ed.putString("textlastword", text.getText().toString());
+                    ed.commit();
+                    ed.putLong("lastdate", currentMillis);
+                    ed.commit();
 
-                    String t = text.getText().toString();
+
 
                     VKAttachments vka = new VKAttachments();
                     String myIDasString = VKSdk.getAccessToken().userId;
                     int myID = Integer.parseInt(myIDasString);
-
 
                     makePost(vka, "testing is 300 bucks", myID);
                 }});
